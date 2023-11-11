ebb89a94c0a2e43a72359391667d13888a435c13
ASPRAJNA <asprajna@gmail.com>
2023-11-11 20:40:37 +0530

    design1

---
+++ script.js
@@ -0,0 +1,31 @@
+function fun1(){
+    document.getElementById('list1').style.color="rgb(141, 66, 173)";
+    document.getElementById('list2').style.color="gray";
+    document.getElementById('list3').style.color="gray";
+    document.getElementById('list4').style.color="gray";
+    document.getElementById('imgtab').src="img1.png";
+}
+
+function fun2(){
+    document.getElementById('list2').style.color="rgb(141, 66, 173)";
+    document.getElementById('list3').style.color="gray";
+    document.getElementById('list4').style.color="gray";
+    document.getElementById('list1').style.color="gray";
+    document.getElementById('imgtab').src="img2.png";
+}
+
+function fun3(){
+    document.getElementById('list3').style.color="rgb(141, 66, 173)";
+    document.getElementById('list2').style.color="gray";
+    document.getElementById('list1').style.color="gray";
+    document.getElementById('list4').style.color="gray";
+    document.getElementById('imgtab').src="img3.png";
+}
+
+function fun4(){
+    document.getElementById('list4').style.color="rgb(141, 66, 173)";
+    document.getElementById('list2').style.color="gray";
+    document.getElementById('list3').style.color="gray";
+    document.getElementById('list1').style.color="gray";
+    document.getElementById('imgtab').src="img4.png";
+}
