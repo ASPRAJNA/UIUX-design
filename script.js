231404ccca08800987204e210291a1e11c554045
ASPRAJNA <asprajna@gmail.com>
2023-11-13 13:34:12 +0530

    Image Carousal

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const panels = document.querySelectorAll('.panel')
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('prev')
+const rightBtn = document.getElementById('next')
 
-panels.forEach(panel=>{
-    panel.addEventListener('click', () => {
-        removeActiveclass()
-        panel.classList.add('active')
-    })
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 3000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 3000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
 })
 
-function removeActiveclass() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
